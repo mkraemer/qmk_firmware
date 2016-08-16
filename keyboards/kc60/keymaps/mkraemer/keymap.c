6c52528d013cf3496f576e51b3d6d0d830766e80
Marius Krämer <marius@marius-kraemer.de>
2016-08-16 21:25:51 +0200

    CGL: Reformat documented layout

--- keyboards/kc60/keymaps/mkraemer/keymap.c
+++ keyboards/kc60/keymaps/mkraemer/keymap.c
@@ -10,25 +10,25 @@
 #define DOT M(_DE_DOT)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-  /*
+  /* Standard-ish QWERTZ
    * ,-----------------------------------------------------------.
-   * |Esc~| 1|  2|  3|  4|  5|  6|  7|  8|  9|  0|  ß| ´ |Backsp |
+   * | ^ | 1|  2|  3|  4|  5|  6|  7|  8|  9|  0|  ß| ´ | Backsp |
    * |-----------------------------------------------------------|
-   * |Tab  |  Q|  W|  E|  R|  T|  Z|  U|  I|  O|  P| Ü | + |  #  |
+   * |Tab  | Q | W | E | R | T | Z | U | U | O | P | Ü | + |  #  |
    * |-----------------------------------------------------------|
-   * |Esc/FN |  A|  S|  D|  F|  G|  H|  J|  K|  L| Ö | Ä |Return |
+   * |Esc/FN | A | S | D | F | G | H | J | K | L | Ö | Ä |Return |
    * |-----------------------------------------------------------|
-   * |Shift   |  Y|  X|  C|  V|  B|  N|  M| , | . | - |    Shift |
+   * |Shift   | Y | X | C | V | B | N | M | , | . | - |    Shift |
    * |-----------------------------------------------------------|
-   * |Ctrl|Gui |Alt |      Space            |Alt |Gui |Ctrl|  FN |
+   * |Ctrl|Gui |Alt |      Space            |Alt |Gui |Ctrl| FN1 |
    * `-----------------------------------------------------------'
    */
   [0] = KEYMAP(
-      DE_CIRC,  KC_1,     KC_2,     KC_3,    KC_4,     KC_5,     KC_6,     KC_7,    KC_8,  KC_9,     KC_0,     DE_SS,    DE_ACUT,  KC_BSPC,  \
-      KC_TAB,   KC_Q,     KC_W,     KC_E,    KC_R,     KC_T,     DE_Z,     KC_U,    KC_I,  KC_O,     KC_P,     DE_UE,    DE_PLUS,  DE_HASH,  \
-      KC_FN0,   KC_A,     KC_S,     KC_D,    KC_F,     KC_G,     KC_H,     KC_J,    KC_K,  KC_L,     DE_OE,    DE_AE,    XXXXXXX,  KC_ENT,   \
-      KC_LSFT,  XXXXXXX,  DE_Y,     KC_X,    KC_C,     KC_V,     KC_B,     KC_N,    KC_M,  COMM,     DOT,      DE_MINS,  XXXXXXX,  KC_RSFT,  \
-      KC_LCTL,  KC_LGUI,  KC_LALT,                     KC_SPC,                                       KC_RALT,  KC_RGUI,  KC_RCTL,  MO(1)     \
+      DE_CIRC, KC_1   , KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0 , DE_SS  , DE_ACUT, KC_BSPC, \
+      KC_TAB , KC_Q   , KC_W, KC_E, KC_R, KC_T, DE_Z, KC_U, KC_I, KC_O, KC_P , DE_UE  , DE_PLUS, DE_HASH, \
+      KC_FN0 , KC_A   , KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, DE_OE, DE_AE  , XXXXXXX, KC_ENT , \
+      KC_LSFT, XXXXXXX, DE_Y, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, COMM, DOT  , DE_MINS, XXXXXXX, KC_RSFT, \
+      KC_LCTL, KC_LGUI, KC_LALT,                     KC_SPC,          KC_RALT, KC_RGUI, KC_RCTL, DF(2)    \
       ),
   /*
    *
@@ -45,11 +45,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * `-----------------------------------------------------------'
    */
   [1] = KEYMAP(
-      BL_STEP,  KC_F1,    KC_F2,    KC_F3,     KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,    KC_F12,  KC_DEL,   \
-      _______,  _______,  KC_UP,    _______,   _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,   _______, _______,  \
-      _______,  KC_LEFT,  KC_DOWN,  KC_RIGHT,  _______,  _______,  KC_LEFT,  KC_DOWN,  KC_UP,    KC_RIGHT, KC_DOWN,  _______,   _______, _______,  \
-      _______,  _______,  _______,  _______,   _______,  KC_VOLD,  KC_VOLU,  KC_MUTE,  _______,  _______,  _______,  _______,   _______, _______,  \
-      _______,  _______,  _______,                       KC_SPC,                                           DEBUG,    RESET,     _______, _______   \
+      BL_STEP, KC_F1  , KC_F2  , KC_F3   , KC_F4  , KC_F5  , KC_F6  , KC_F7  , KC_F8  , KC_F9   , KC_F10 , KC_F11 , KC_F12 , KC_DEL , \
+      _______, _______, KC_UP  , _______ , _______, _______, _______, _______, _______, _______ , _______, _______, _______, _______, \
+      _______, KC_LEFT, KC_DOWN, KC_RIGHT, _______, _______, KC_LEFT, KC_DOWN, KC_UP  , KC_RIGHT, KC_DOWN, _______, _______, _______, \
+      _______, _______, _______, _______ , _______, KC_VOLD, KC_VOLU, KC_MUTE, _______, _______ , _______, _______, _______, _______, \
+      _______,  _______,  _______,                            KC_SPC,                               DEBUG,   RESET, _______, _______  \
       ),
 };
 
