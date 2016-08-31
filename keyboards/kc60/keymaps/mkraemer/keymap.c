2990a41f8f69182ee425105cb6fc88fa83e62866
Marius Krämer <marius@marius-kraemer.de>
2016-08-31 17:30:27 +0200

    Implement adnw layers

--- keyboards/kc60/keymaps/mkraemer/keymap.c
+++ keyboards/kc60/keymaps/mkraemer/keymap.c
@@ -28,7 +28,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_TAB , KC_Q   , KC_W, KC_E, KC_R, KC_T, DE_Z, KC_U, KC_I, KC_O, KC_P , DE_UE  , DE_PLUS, DE_HASH, \
       KC_FN0 , KC_A   , KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, DE_OE, DE_AE  , XXXXXXX, KC_ENT , \
       KC_LSFT, XXXXXXX, DE_Y, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, COMM, DOT  , DE_MINS, XXXXXXX, XXXXXXX, \
-      KC_LCTL      , KC_LGUI, KC_LALT,         LT(2,KC_SPC),          KC_RALT, KC_RGUI, KC_RCTL, DF(3)    \
+      KC_LCTL      , KC_LGUI, KC_LALT,         LT(5,KC_SPC),          KC_RALT, KC_RGUI, KC_RCTL, DF(2)    \
       ),
 
   /*
@@ -51,26 +51,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       _______, _______   , _______   , _______    , _______, KC_VOLD, KC_VOLU, KC_MUTE, _______, _______ , _______, _______, _______, _______, \
       _______,  _______,  _______,                                 KC_SPC,                                   DEBUG,   RESET, _______, _______  \
       ),
-  /*
-   * ,-----------------------------------------------------------.
-   * |   | F1| F2| F3| F4| F5| F6| F7| F8| F9|F10|F11|F12|  DEL  |
-   * |-----------------------------------------------------------|
-   * |     |   |MUP|   |   |   |   | M1| M3| M2|   |   |   |     |
-   * |-----------------------------------------------------------|
-   * |       |MLT|MDW|MRT|   |   |LFT|DWN| UP|RGT|   |   |       |
-   * |-----------------------------------------------------------|
-   * |        |   |   |   | V-| V+|MTE|   |   |   |   |          |
-   * |-----------------------------------------------------------|
-   * |    |    |    |      Space            |DBG |RST |    |     |
-   * `-----------------------------------------------------------'
-   */
-  [2] = KEYMAP(
-      _______, LGUI(KC_1), LGUI(KC_2), LGUI(KC_3), LGUI(KC_4), LGUI(KC_5), LGUI(KC_6), LGUI(KC_7), LGUI(KC_8), LGUI(KC_9), _______, _______, _______, _______, \
-      _______, LALT(KC_J), LGUI(KC_K), LALT(KC_K), _______   , _______   , _______   , _______   , _______   , _______   , _______, _______, _______, _______, \
-      _______, LGUI(KC_H), LGUI(KC_J), LGUI(KC_L), _______   , _______   , _______   , _______   , _______   , _______   , _______, _______, _______, LGUI(KC_ENT), \
-      _______, _______   , _______   , _______   , _______   , _______   , _______   , _______   , _______   , _______   , _______, _______, _______, _______, \
-      _______, _______   , _______   ,                      _______                                             , _______   , _______   , _______   , _______  \
-      ),
 
   /* ADNW-inspired
    * ,-----------------------------------------------------------.
@@ -85,22 +65,80 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |Ctrl|Gui |Alt |      Space            |Alt |Gui |Ctrl| FN0 |
    * `-----------------------------------------------------------'
    */
+  [2] = KEYMAP(
+      DE_CIRC, KC_1   , KC_2, KC_3 , KC_4  , KC_5   , KC_6, KC_7, KC_8, KC_9, KC_0, DE_MINS, XXXXXXX, KC_BSPC, \
+      KC_TAB , DE_K   , DE_U, DE_UE, KC_DOT, DE_AE  , DE_V, DE_G, DE_C, DE_L, DE_J, DE_F   , MO(4), XXXXXXX, \
+      MO(4), DE_H   , DE_I, DE_E , DE_A  , DE_O   , DE_D, DE_T, DE_R, DE_N, DE_S, DE_SS  , XXXXXXX, KC_ENT , \
+      MO(3)  , XXXXXXX, DE_X, DE_Y , DE_OE , KC_COMM, DE_Q, DE_B, DE_P, DE_W, DE_M, DE_Z   , XXXXXXX, MO(3)  , \
+      KC_LCTL, KC_LGUI, KC_LALT,                     LT(5,KC_SPC),             KC_RALT, KC_RGUI, KC_RCTL, DF(0)    \
+      ),
+
+  /* ADNW-inspired shift
+   * ,-----------------------------------------------------------.
+   * | ^ | 1|  2|  3|  4|  5|  6|  7|  8|  9|  0|  ß|   | Backsp |
+   * |-----------------------------------------------------------|
+   * |Tab  | K | U | Ü | . | Ä | V | G | C | L | J | F |   |     |
+   * |-----------------------------------------------------------|
+   * |       | H | I | E | A | O | D | T | R | S | ß |   |Return |
+   * |-----------------------------------------------------------|
+   * |Shift   | X | Y | Ö | , | Q | B | P | W | M | Z |    Shift |
+   * |-----------------------------------------------------------|
+   * |Ctrl|Gui |Alt |      Space            |Alt |Gui |Ctrl| FN0 |
+   * `-----------------------------------------------------------'
+   */
   [3] = KEYMAP(
-      DE_CIRC, KC_1   , KC_2, KC_3 , KC_4  , KC_5   , KC_6, KC_7, KC_8, KC_9, KC_0, DE_SS, DE_ACUT, KC_BSPC, \
-      KC_TAB , DE_K   , DE_U, DE_UE, KC_DOT, DE_AE  , DE_V, DE_G, DE_C, DE_L, DE_J, DE_F , XXXXXXX, XXXXXXX, \
-      XXXXXXX, DE_H   , DE_I, DE_E , DE_A  , DE_O   , DE_D, DE_T, DE_R, DE_N, DE_S, DE_SS, XXXXXXX, KC_ENT , \
-      KC_LSFT, XXXXXXX, DE_X, DE_Y , DE_OE , KC_COMM, DE_Q, DE_B, DE_P, DE_W, DE_M, DE_Z , XXXXXXX, KC_RSFT, \
-      KC_LCTL, KC_LGUI, KC_LALT,                     KC_SPC,             KC_RALT, KC_RGUI, KC_RCTL, DF(0)    \
+      DE_CIRC, S(DE_CIRC), S(KC_3), XXXXXXX , UC(0x00bb), UC(0x00ab), S(KC_4), DE_EURO, UC(0x201E), UC(0x201c), _______, UC(0x2014), DE_ACUT, KC_BSPC, \
+      KC_TAB , S(DE_K)   , S(DE_U), S(DE_UE), KC_DOT    , S(DE_AE)  , S(DE_V), S(DE_G), S(DE_C)   , S(DE_L)   , S(DE_J), S(DE_F)   , MO(4)  , XXXXXXX, \
+      _______, S(DE_H)   , S(DE_I), S(DE_E) , S(DE_A)   , S(DE_O)   , S(DE_D), S(DE_T), S(DE_R)   , S(DE_N)   , S(DE_S), UC(0x1e9e), XXXXXXX, KC_ENT , \
+      _______, XXXXXXX   , S(DE_X), S(DE_Y) , S(DE_OE)  , DE_MINS   , S(DE_Q), S(DE_B), S(DE_P)   , S(DE_W)   , S(DE_M), S(DE_Z)   , XXXXXXX, _______, \
+      _______,_______,_______,                            KC_SPC,                       _______,_______,_______, _______    \
+      ),
+
+  /* ADNW-inspired meta
+   * ,-----------------------------------------------------------.
+   * | ^ | 1|  2|  3|  4|  5|  6|  7|  8|  9|  0|  ß|   | Backsp |
+   * |-----------------------------------------------------------|
+   * |Tab  | K | U | Ü | . | Ä | V | G | C | L | J | F |   |     |
+   * |-----------------------------------------------------------|
+   * |       | H | I | E | A | O | D | T | R | S | ß |   |Return |
+   * |-----------------------------------------------------------|
+   * |Shift   | X | Y | Ö | , | Q | B | P | W | M | Z |    Shift |
+   * |-----------------------------------------------------------|
+   * |Ctrl|Gui |Alt |      Space            |Alt |Gui |Ctrl| FN0 |
+   * `-----------------------------------------------------------'
+   */
+  [4] = KEYMAP(
+      _______, _______     , _______   , _______, _______, _______   , _______, _______, _______, _______, _______, _______, _______, KC_BSPC, \
+      _______, RALT(KC_DOT), S(DE_MINS), DE_LBRC, DE_RBRC, DE_CIRC   , DE_EXLM, DE_LESS, DE_MORE, DE_EQL , DE_AMPR, _______, XXXXXXX, XXXXXXX, \
+      _______, DE_BSLS     , S(KC_7)   , DE_LCBR, DE_RCBR, S(DE_PLUS), DE_QST , DE_LPRN, DE_RPRN, DE_MINS, DE_COLN, DE_AT  , XXXXXXX, KC_ENT , \
+      _______, XXXXXXX     , DE_HASH   , DE_DLR , DE_PIPE  , DE_TILD, XXXXXXX, DE_PLUS, DE_PERC, DE_DQOT, DE_QUOT,  DE_SCLN, XXXXXXX, _______, \
+      _______, _______, KC_LALT,                            KC_SPC,                       KC_RALT, KC_RGUI, KC_RCTL, _______    \
+      ),
+  /*
+   * ,-----------------------------------------------------------.
+   * |   | F1| F2| F3| F4| F5| F6| F7| F8| F9|F10|F11|F12|  DEL  |
+   * |-----------------------------------------------------------|
+   * |     |   |MUP|   |   |   |   | M1| M3| M2|   |   |   |     |
+   * |-----------------------------------------------------------|
+   * |       |MLT|MDW|MRT|   |   |LFT|DWN| UP|RGT|   |   |       |
+   * |-----------------------------------------------------------|
+   * |        |   |   |   | V-| V+|MTE|   |   |   |   |          |
+   * |-----------------------------------------------------------|
+   * |    |    |    |      Space            |DBG |RST |    |     |
+   * `-----------------------------------------------------------'
+   */
+  [5] = KEYMAP(
+      _______, LGUI(KC_1), LGUI(KC_2), LGUI(KC_3), LGUI(KC_4), LGUI(KC_5), LGUI(KC_6), LGUI(KC_7), LGUI(KC_8), LGUI(KC_9), _______, _______, _______, _______, \
+      _______, LALT(KC_J), LGUI(KC_K), LALT(KC_K), _______   , _______   , _______   , _______   , _______   , _______   , _______, _______, _______, _______, \
+      _______, LGUI(KC_H), LGUI(KC_J), LGUI(KC_L), _______   , _______   , _______   , _______   , _______   , _______   , _______, _______, _______, LGUI(KC_ENT), \
+      _______, _______   , _______   , _______   , _______   , _______   , _______   , _______   , _______   , _______   , _______, _______, _______, _______, \
+      _______, _______   , _______   ,                      _______                                             , _______   , _______   , _______   , _______  \
       ),
 };
 
 const uint16_t PROGMEM fn_actions[] = {
     [0]  = ACTION_LAYER_TAP_KEY(1, KC_ESC),  // Layer 1 when hold, esc when tapped
 };
-
-static void handle_adnw_key(keyrecord_t *record, uint8_t id)
-{
-}
 
 const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt)
 {
@@ -139,4 +177,8 @@ const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt)
     }
 
     return MACRO_NONE;
+};
+
+void matrix_init_user(void) {
+  set_unicode_input_mode(UC_LNX);
 };
