811cf443c5e66fa6bc61f9981f7dfcdb7eb6b3e1
Marius Krämer <marius@marius-kraemer.de>
2016-11-16 18:42:16 +0100

    Fix right shift

--- keyboards/kc60/keymaps/mkraemer/keymap.c
+++ keyboards/kc60/keymaps/mkraemer/keymap.c
@@ -72,7 +72,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       DE_CIRC, KC_1   , KC_2   , KC_3, KC_4   , KC_5   , KC_6   , KC_7   , KC_8      , KC_9, KC_0 , DE_SS  , DE_ACUT, KC_BSPC, \
       TAB , KC_Q   , KC_W   , KC_E, KC_R   , KC_T   , DE_Z   , KC_U   , KC_I      , KC_O, KC_P , DE_UE  , DE_PLUS, DE_HASH, \
       KC_FN0 , KC_A   , KC_S   , KC_D, KC_F   , KC_G   , KC_H   , KC_J   , KC_K      , KC_L, DE_OE, DE_AE  , X, KC_ENT , \
-      KC_LSFT, X, DE_Y   , KC_X, KC_C   , KC_V   , KC_B   , KC_N   , KC_M      , COMM, DOT  , DE_MINS, X, X, \
+      KC_LSFT, X, DE_Y   , KC_X, KC_C   , KC_V   , KC_B   , KC_N   , KC_M      , COMM, DOT  , DE_MINS, X, KC_RSFT, \
       KC_LCTL, KC_LGUI, KC_LALT,                  SPC, X                           , KC_RALT, KC_RGUI, KC_RCTL, DF(2)\
       ),
 
